3e848eb82fde2d763bf47a945ed616f8e29633eb
Milan Radulovic <milan.radulovic@rbt.rs>
2021-07-30 09:53:35 +0200

    Create measurement cards container page #22

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Home } from "./pages/index";
-import { MeasurementCard } from "./components/index";
+import { Home, Averages } from "./pages/index";
 
 const Routes = () => (
   <div className="container">
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/averages" component={MeasurementCard} />
+      <Route path="/averages" component={Averages} />
     </Switch>
   </div>
 );
